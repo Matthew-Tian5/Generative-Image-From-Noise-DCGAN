06faa9fda13fabf2c925344866b594637be3bd66
Matthew-Tian5 <matthew.tian2005@gmail.com>
2026-05-15 15:16:24 -0400

    added the weight initialisation and the generator for the images

--- Dcgan_cifar10.py
+++ Dcgan_cifar10.py
@@ -29,3 +29,52 @@ BETA2        = 0.999
 EPOCHS       = 30
 #checks for GPU otherwise uses CPU
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+#m is a single module passed in by .apply()
+def weights_init(m):
+    #gets teh class name as a string
+    classname = m.__class__.__name__
+    #catches both Conv2d and Contranspose2d and fills the weight tensor with vlaues sampled from N
+    if "Conv" in classname:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    #BatchNorm scale parameter y starts near 1 so it begins as near idenitiy and shifts parameter v starts at 0 so it doesn't shift the mean of the output
+    elif "BatchNorm" in classname:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
+
+
+
+#Generaor is a pytorch model that gives a gradient tracking and everything else pytorch needs to treat it as a trainable model
+
+class Generator(nn.Module):
+
+    
+    def __init__(self):
+        
+        super()._-init__()
+        #its a shorthand so the layer definitions arent a wall of numbers
+        G = CHANNELS_G
+        #chians the layers in order
+        self.net = nn.Sequential(
+            #each layer is a block of ConvTranspose2d, BatchNorm2d, and ReLU except the last layer which is just ConvTranspose2d and Tanh
+            self._block(LATENT_DIM, G * 8, kernel =2, stride = 1, padding = 0), 
+            self._block(G*8, G*4, kernel = 4, stride = 2, padding = 1),
+            self._block(G*4, G*2, kernel = 4, stride =2, padding = 1),
+            self._block(G*2, G, kernel = 4, stride = 2, padding = 1),
+            nn.ConvTranspose2d(G, IMG_CHANNELS, kernel_size = 4, stride = 2, padding = 1),
+            nn.Tanh()
+        
+        )
+
+    #this methods builds and returns a small seequential block
+    @staticmethod
+    def _block(in_c, out_c, kernel, stride, padding):
+        return nn.Sequential(
+            nn.ConvTranspose2d(in_c, out_c, kernel, stride, padding, bias = False),
+            nn.BatchNorm2d(out_c),
+            nn.ReLU()
+        )
+    def forward(self, z):
+        return self.net(z)
